1abf2ff96c2e38ab7641ec6b472f24fcb0065766
Marco Joaquin Po <marco010598@gmail.com>
2019-03-28 23:15:34 +0800

    Softdevcode3
    updates

--- inventory_vue/database/migrations/2019_03_25_071737_create_locations_table.php
+++ inventory_vue/database/migrations/2019_03_25_071737_create_locations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductsTable extends Migration
+class CreateLocationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,14 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        if(Schema::hasTable('locations', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->string(',lastLocation')->default('');
+            $table->string('BU');
+            $table->string('OU');
             $table->timestamps();
-        });
+        }));
     }
 
     /**
@@ -26,6 +30,6 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('locations');
     }
 }
